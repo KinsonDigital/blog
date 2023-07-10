d817e24df105bc094a209571c61e3559adb08c2e
Calvin Wilkinson <kinsondigital@gmail.com>
2023-07-10 15:43:26 +0100

    chore: create custom component for urls that open in a new tab

---
+++ src/components/URL.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import OpenNewTab from '@site/static/img/new-tab.svg';
+
+/**
+ * The parameter object used for this component.
+ */
+interface Params {
+    link: string,
+    text?: string,
+};
+
+/**
+ * Creates a link with an open new tab icon.
+ * @param params The parameters for the component.
+ * @returns The component.
+ */
+export function URL (param: Params) {
+
+    const link = param.link.startsWith("https://")
+        ? param.link
+        : `https://${param.link}`;
+
+    const text = param.text === undefined || param.text === null || param.text === ""
+        ? link
+        : param.text;
+
+	return (
+		<a href={link}
+			target="_blank"
+            rel="noopener noreferrer"
+			className="link">
+			{text}<OpenNewTab style={{paddingTop: '10', paddingLeft: '0.25%', width: '2%'}}/>
+		</a>
+	);
+}
