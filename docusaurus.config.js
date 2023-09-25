46eb3e9dd51ec17581b3dba05345b6d774d8568e
Calvin Wilkinson <kinsondigital@gmail.com>
2023-09-25 17:35:34 +0100

    config: setup blog to run in blog only mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Kinson Digital',
+  tagline: 'Quality Software Development',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -37,10 +37,7 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: 
-        {
-          sidebarPath: require.resolve('./sidebars.js'),
-        },
+        docs: false,
         blog: {
           routeBasePath: '/',
           showReadingTime: true,
