c13b846242e0d2aa8311300f58231d64b7326f00
Calvin Wilkinson <kinsondigital@gmail.com>
2024-04-16 23:23:50 +0100

    chore: create join community custom component

---
+++ src/components/JoinComm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import JoinCommunity from "../join-comm.mdx";
+
+/**
+ * Creates a link with an open new tab icon.
+ * @param params The parameters for the component.
+ * @returns The component.
+ */
+const MarkdownComp: React.FC = () => {
+  return (
+    <JoinCommunity/>
+  );
+}
+
+export default MarkdownComp;
